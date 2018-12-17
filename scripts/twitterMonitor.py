a78a555a389568e7889b0c71e3fc67f0b9a41720
cyberh99 <hromerolopez@protonmail.com>
2018-12-17 15:38:13 +0100

    Script monitoreo Twitter v1 done

--- scripts/twitterMonitor.py
+++ scripts/twitterMonitor.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+import os
+from dotenv import load_dotenv
+import sys
+
+if len(sys.argv) != 2:
+    print("[!]twitterMonitor.py username")
+    exit(-1)
 
 try:
     import tweepy
@@ -7,11 +14,19 @@ except ImportError:
 
 """Authentication process and API creation"""
 def createAPI():
+    load_dotenv()
+    auth=tweepy.OAuthHandler(os.environ["API_KEY"],os.environ["API_KEY_SECRET"])
+    auth.set_access_token(os.environ["ACCESS_TOKEN"],os.environ["ACCESS_TOKEN_SECRET"])
+    return tweepy.API(auth)
 
 class customListener(tweepy.StreamListener):
 
     def on_status(self,status):
         print(status.text)
 
+twitterAPI = createAPI()
 listener = customListener()
-stream = tweepy.Stream(auth)
+userID = twitterAPI.get_user(sys.argv[1]).id
+stream = tweepy.Stream(auth=twitterAPI.auth,listener=customListener())
+print(userID)
+stream.filter(follow=[str(userID)])
